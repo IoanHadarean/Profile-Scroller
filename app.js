2286cae6456d532e614578c7d573dad12f7695fb
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-16 09:52:52 +0000

    Added reload for page when there are no more profiles

--- app.js
+++ app.js
@@ -38,39 +38,42 @@ document.getElementById('next').addEventListener('click', nextProfile);
 
 // Next Profile Display
 function nextProfile() {
-    
+
     const currentProfile = profiles.next().value;
-    
-    document.getElementById('profileDisplay').innerHTML = `
-    <ul class = "list-group">
-        <li class="list-group-item">Name: ${currentProfile.name}</li>
-    </ul>
-     <ul class = "list-group">
-        <li class="list-group-item">Age: ${currentProfile.age}</li>
-    </ul>
-     <ul class = "list-group">
-        <li class="list-group-item">Location: ${currentProfile.location}</li>
-    </ul>
-     <ul class = "list-group">
-        <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
-    </ul>
-    `;
-    
-    document.getElementById('imageDisplay').innerHTML = `
-        <img src="${currentProfile.image}">
-    `;
+
+    if (currentProfile !== undefined) {
+        document.getElementById('profileDisplay').innerHTML = `
+        <ul class = "list-group">
+            <li class="list-group-item">Name: ${currentProfile.name}</li>
+        </ul>
+        <ul class = "list-group">
+            <li class="list-group-item">Age: ${currentProfile.age}</li>
+        </ul>
+        <ul class = "list-group">
+            <li class="list-group-item">Location: ${currentProfile.location}</li>
+        </ul>
+        <ul class = "list-group">
+            <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+        </ul>
+        `;
+
+        document.getElementById('imageDisplay').innerHTML = `
+        <img src="${currentProfile.image}">`;
+    }
+    else {
+        // No more profiles
+        window.location.reload();
+    }
 }
 
 
 // Profile Iterator
 function profileIterator(profiles) {
     let nextIndex = 0;
-    
+
     return {
         next: function() {
-            return nextIndex < profiles.length ?
-            {value: profiles[nextIndex++], done: false}:
-            {done: true};
+            return nextIndex < profiles.length ? { value: profiles[nextIndex++], done: false } : { done: true };
         }
     };
 }
